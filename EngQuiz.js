da8999248a5173d4519dec32a61fc303eb96dce4
SOHYUN <48352775gh@gmail.com>
2023-03-16 20:12:19 +0900

    Basic HTML and start function

---
+++ EngQuiz.js
@@ -0,0 +1,14 @@
+
+let ProfileAndStart = document.getElementById('ProfileAndStart');
+
+
+let name = document.getElementById('name');
+
+let startButton = document.getElementById('startButton');
+
+let QuizSection = document.getElementById('QuizSection');
+
+function startButtonClick() {
+    ProfileAndStart.style.display = "none";
+    QuizSection.style.display = "block"
+   }
